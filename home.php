96d4ecf612b284405ba8c7f35e10ceedf328deeb
emircarikci <147662209+emircarikci@users.noreply.github.com>
2024-05-28 21:37:56 +0300

    Delete home.php

--- home.php
+++
@@ -1,8 +0,0 @@
-<div class="bg">
-    <div class="container text-center">
-        <h1 class="mb-4">Hoş Geldiniz</h1>
-        <p class="lead mb-4">Web sitemize hoş geldiniz! Lütfen giriş yapın veya kayıt olun.</p>
-        <a href="index.php?page=giris" class="btn btn-custom btn-lg mb-2">Giriş Yap</a>
-        <a href="index.php?page=kayit_ol" class="btn btn-secondary btn-lg">Kayıt Ol</a>
-    </div>
-</div>
